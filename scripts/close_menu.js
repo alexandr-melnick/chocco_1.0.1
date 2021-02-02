a6f1b34671051b87d2ea84bb30c11ffed90da6bd
Aleksandr Melnick <alexksandr.melnick@gmail.com>
2021-02-02 18:42:58 +0300

    animation screen menu

--- scripts/close_menu.js
+++ scripts/close_menu.js
@@ -1,12 +1,17 @@
 const hamburg = document.querySelector(".hamburger");
 const mClose = document.querySelector(".screen-menu__close");
 const mScreen = document.querySelector(".screen-menu");
+const body = document.body;
 
 hamburg.addEventListener("click", evt => {
   evt.preventDefault();
-  mScreen.style.display = "flex";
+  mScreen.style.top = "0";
+  mScreen.style.opacity = "1";
+  body.style.overflow = "hidden";
 })
 mClose.addEventListener("click", evt => {
   evt.preventDefault();
-  mScreen.style.display = "none";
+  mScreen.style.top = "-100%";
+  mScreen.style.opacity = "0";
+  body.style.overflow = "scroll";
 })
