0dbc32e813552f210a4b16f66e50a3a96acc0d2d
Aleksandr Melnick <alexksandr.melnick@gmail.com>
2021-02-14 16:10:53 +0300

    add OPS

--- src/scripts/close_menu.js
+++ src/scripts/close_menu.js
@@ -2,6 +2,7 @@
   const hamburg = document.querySelector(".hamburger");
   const mClose = document.querySelector(".screen-menu__close");
   const mScreen = document.querySelector(".screen-menu");
+  const menuItem = document.querySelector(".screen-menu__link");
   const body = document.body;
 
   hamburg.addEventListener("click", evt => {
@@ -14,6 +15,10 @@
     evt.preventDefault();
     mScreen.style.top = "-100%";
     mScreen.style.opacity = "0";
-    body.style.overflow = "scroll";
+  })
+  menuItem.addEventListener("click", evt => {
+    evt.preventDefault();
+    mScreen.style.top = "-100%";
+    mScreen.style.opacity = "0";
   })
 })();
--- src/scripts/ops.js
+++ src/scripts/ops.js
@@ -1,23 +1,104 @@
 (function () {
-  const section = $(".section");
+  const section = $("section");
   const display = $(".maincontent");
 
-  const performTransition = sectNum => {
-    const posit = sectNum * -100;
+  let inScroll = false;
 
-    display.css({
-      tansform: `translateY(${posit}%)`
-    })
+  section.first().addClass("active");
+
+  const performTransition = sectEq => {
+    if (inScroll == false) {
+      const posit = sectEq * -100;
+      inScroll = true;
+
+      const currentSect = section.eq(sectEq);
+      const themeSect = currentSect.attr("data-theme");
+      const menuFix = $(".fixed-menu");
+      if (themeSect == "white") {
+        menuFix.removeClass("fixed-menu--darkgreen")
+      } else(
+        menuFix.addClass("fixed-menu--darkgreen")
+      )
+      menuFix
+        .find(".fixed-menu__item")
+        .eq(sectEq)
+        .addClass("fixed-menu__item--active")
+        .siblings()
+        .removeClass("fixed-menu__item--active");
+
+      display.css({
+        transform: `translateY(${posit}%)`
+      })
+      section.eq(sectEq).addClass("active").siblings().removeClass("active");
+      setTimeout(() => {
+        inScroll = false;
+      }, 1000)
+    }
+
+  }
+
+  const scrollViewport = direction => {
+    const actSection = section.filter(".active");
+    const nextSection = actSection.next();
+    const prevSection = actSection.prev();
+
+    if (direction == "next" && nextSection.length) {
+      performTransition(nextSection.index())
+    }
+    if (direction == "prev" && prevSection.length) {
+      performTransition(prevSection.index())
+    }
+
+
   }
 
   $(window).on("wheel", e => {
     const deltaY = e.originalEvent.deltaY;
-    // console.log(deltaY);
-
     if (deltaY > 0) {
-      performTransition(2);
-    } else {
+      scrollViewport("next");
+    }
+    if (deltaY < 0) {
+      scrollViewport("prev");
+    }
 
+  })
+
+  $(window).on("keydown", e => {
+    const key = e.keyCode;
+    const tagName = e.target.tagName.toLowerCase();
+
+    if (tagName !== "input" && tagName !== "textarea") {
+      if (key == 38) {
+        scrollViewport("prev");
+      }
+
+      if (key == 40) {
+        scrollViewport("next");
+      }
     }
   })
+
+  $(".wrapper").on("touchmove", e => e.preventDefault())
+
+  $("[data-scroll-to]").on("click", e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const target = $this.attr("data-scroll-to");
+    const reqSection = $(`[data-section-id=${target}]`);
+
+    performTransition(reqSection.index());
+  })
+
+  $("body").swipe({
+    swipe: function (event, direction) {
+      if (direction === "up") {
+        scrollViewport("next");
+      }
+      if (direction === "down") {
+        scrollViewport("prev");
+      }
+    }
+
+  });
 })();
